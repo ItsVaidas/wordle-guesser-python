93f8548aabebfdc04ce80dfb2840aa1c54fc040f
ItsVaidas <91265795+ItsVaidas@users.noreply.github.com>
2024-07-28 03:50:22 +0300

    Added a simple toggle to switch beatween simulation mode and user input mode

--- bot.py
+++ bot.py
@@ -59,21 +59,57 @@ def choose_next_word(chosen_words, correct_positions, incorrect_positions, corre
     
     return random.choice(possible_words)
 
-
+simulate = True
 
 if __name__ == "__main__":
 
     file = open("sgb-words.txt", "r")
     words = file.readlines()
 
-    count = 0
-    average_guesses = 0.0
-    guessed_frequency = {}
+    if simulate:
+        count = 0
+        average_guesses = 0.0
+        guessed_frequency = {}
 
-    for simulate_word in words:
-        simulate_word = simulate_word.strip()
+        for simulate_word in words:
+            simulate_word = simulate_word.strip()
 
+            chosen_words = []
+            correct_positions = []
+            incorrect_positions = []
+            correct_letters = []
+            incorrect_letters = []
+            
+            word = random_word(words)
 
+            for i in range(12):
+                chosen_words.append(word)
+
+                correct_positions.append([position for position in range(5) if simulate_word[position] == word[position]])
+
+                incorrect_positions.append([position for position in range(5) if simulate_word[position] != word[position]])
+
+                correct_letters.append([letter for letter in word if simulate_word.count(letter) > 0])
+
+                incorrect_letters.append([letter for letter in word if simulate_word.count(letter) == 0])
+
+                word = choose_next_word(chosen_words, correct_positions, incorrect_positions, correct_letters, incorrect_letters, words)
+
+                if word == simulate_word:
+                    average_guesses += i + 1
+                    count += 1
+                    guessed_frequency[i + 1] = guessed_frequency.get(i + 1, 0) + 1
+                    break
+            
+            guessed_frequency = dict(sorted(guessed_frequency.items()))
+
+            print("Average guesses: ", average_guesses / count, "\r", end="")
+            print("Guessed frequency: ", guessed_frequency, "\r", end="")
+    
+        print("Average guesses: ", average_guesses / count)
+        print("Guessed frequency: ", guessed_frequency)
+
+    else:
         chosen_words = []
         correct_positions = []
         incorrect_positions = []
@@ -82,54 +118,27 @@ if __name__ == "__main__":
 
         word = random_word(words)
 
-        # for i in range(5):
-        #     chosen_words.append(word)
-
-        #     print(word)
-
-        #     input_correct_positions = input("Enter the number of correct positions by comma: ")
-        #     if input_correct_positions != "":
-        #         correct_positions.append(input_correct_positions.split(","))
-
-        #     input_incorrect_positions = input("Enter the number of incorrect positions by comma: ")
-        #     if input_incorrect_positions != "":
-        #         incorrect_positions.append(input_incorrect_positions.split(","))
-
-        #     input_correct_letters = input("Enter the number of correct letters by comma: ")
-        #     if input_correct_letters != "":
-        #         correct_letters.append(input_correct_letters.split(","))
-
-        #     input_incorrect_letters = input("Enter the number of incorrect letters by comma: ")
-        #     if input_incorrect_letters != "":
-        #         incorrect_letters.append(input_incorrect_letters.split(","))
-
-        #     word = choose_next_word(chosen_words, correct_positions, incorrect_positions, correct_letters, incorrect_letters, words)
-
         for i in range(12):
             chosen_words.append(word)
 
-            correct_positions.append([position for position in range(5) if simulate_word[position] == word[position]])
+            print(word)
 
-            incorrect_positions.append([position for position in range(5) if simulate_word[position] != word[position]])
+            input_correct_positions = input("Enter the number of correct positions by comma: ")
+            if input_correct_positions != "":
+                correct_positions.append(input_correct_positions.split(","))
 
-            correct_letters.append([letter for letter in word if simulate_word.count(letter) > 0])
+            input_incorrect_positions = input("Enter the number of incorrect positions by comma: ")
+            if input_incorrect_positions != "":
+                incorrect_positions.append(input_incorrect_positions.split(","))
 
-            incorrect_letters.append([letter for letter in word if simulate_word.count(letter) == 0])
+            input_correct_letters = input("Enter the number of correct letters by comma: ")
+            if input_correct_letters != "":
+                correct_letters.append(input_correct_letters.split(","))
+
+            input_incorrect_letters = input("Enter the number of incorrect letters by comma: ")
+            if input_incorrect_letters != "":
+                incorrect_letters.append(input_incorrect_letters.split(","))
 
             word = choose_next_word(chosen_words, correct_positions, incorrect_positions, correct_letters, incorrect_letters, words)
-
-            if word == simulate_word:
-                average_guesses += i + 1
-                count += 1
-                guessed_frequency[i + 1] = guessed_frequency.get(i + 1, 0) + 1
-                break
-        
-        guessed_frequency = dict(sorted(guessed_frequency.items()))
-
-        print("Average guesses: ", average_guesses / count, "\r", end="")
-        print("Guessed frequency: ", guessed_frequency, "\r", end="")
-    
-    print("Average guesses: ", average_guesses / count)
-    print("Guessed frequency: ", guessed_frequency)
 
     
